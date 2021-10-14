26ebd3ff0802993e8e42127f4351cfeb645d7262
malinovaya0iriska <malinovayairiska@gmail.com>
2021-10-14 21:44:01 +0300

    feat: add adaptive

--- src/a6-footer/Footer.js
+++ src/a6-footer/Footer.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import styleContainer from "../common/styles/containerStyle.module.scss";
 import styles from './Footer.module.scss'
 import {BsLinkedin, SiCodewars, SiGithubsponsors, VscGithub} from "react-icons/all";
 
 export const Footer = () => {
     return (
         <div className={styles.footerBlock}>
-            <div className={`${styleContainer.container} ${styles.footerContainer}`}>
+            <div className={styles.footerContainer}>
                 <ul className={styles.socialShare}>
                     <li><a href={'https://github.com/malinovaya0iriska'}><VscGithub/></a></li>
                     <li><a href={'https://www.codewars.com/users/malinovaya0iriska'}><SiCodewars/></a></li>
                     <li><a href={'https://www.linkedin.com/in/irina-martsulevich-342073217/'}><BsLinkedin/></a></li>
                 </ul>
-                <h4>Made with <SiGithubsponsors/> by Iryna Martsulevich</h4>
-                <span>©2021 All rights reserved</span>
+                <div>
+                    <h4>Made with <SiGithubsponsors/> by Iryna Martsulevich</h4>
+                    <span>©2021 All rights reserved</span>
+                </div>
+
             </div>
+
+
         </div>
     )
 };
